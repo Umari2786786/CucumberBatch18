b75e4b4b3063f9fcb30bc043d3f4dff34964c86c
umari2786786 <oiqbal2786@gmail.com>
2024-01-31 01:58:49 +0000

    31/01/24

--- src/test/java/runners/RunnerClass.java
+++ src/test/java/runners/RunnerClass.java
@@ -4,12 +4,12 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-//We need runtih options to run with junit
+//We need run with options to run with junit
 @RunWith(Cucumber.class)
 //cucumber options decide what to execute, where to execute
 @CucumberOptions(
         //features we use to provide the path of the feature file
-        features = "src/test/resources/Features/",
+        features = "src/test/resources/Features/Login.feature",
         //glue below will link the above to the below
         glue = "steps"
 
